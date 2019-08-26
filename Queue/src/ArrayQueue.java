74599d07e5f16d49a5753189eb97b8993b0635b6
Coooolbreeze <392113643@qq.com>
2019-08-26 17:14:04 +0800

    used LinkedList to support Queue

--- Queue/src/ArrayQueue.java
+++ Queue/src/ArrayQueue.java
@@ -59,10 +59,11 @@ public class ArrayQueue<E> implements Queue<E> {
         for (int i = 0; i < 10; i++) {
             queue.enqueue(i);
             System.out.println(queue);
+
+            if (i % 3 == 2) {
+                queue.dequeue();
+                System.out.println(queue);
+            }
         }
-
-        System.out.println(queue.dequeue());
-
-        System.out.println(queue);
     }
 }
